c381b05ad9b75af95c0437e9ada94ff9b622dd76
Asadulla Juraev <tezkormedia@gmail.com>
2021-05-13 09:30:06 +0300

    Initial Commit

---
+++ MyLoan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyLoan
+//
+//  Created by Asadulla Juraev on 13.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
